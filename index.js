7cca771fef0eb982ecc874d5e0170c7973a24fe3
Thiago Allisson <thiago.ribeiro@ifma.edu.br>
2022-06-05 16:59:46 -0300

    Fix signup.ejs

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.use(express.static(__dirname + '/views'));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/index', (req, res) => {
+  res.render('index');
+}) 
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+})
 
 app.listen(port,() => {
     console.log(`Servidor rodando na porta ${port}`);
