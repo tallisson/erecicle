208e9515fac1879e88f67880e621cb065eb0cc05
Thiago Allisson <thiago.ribeiro@ifma.edu.br>
2022-06-05 21:42:10 -0300

    Ajustando path para views

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', './src/views');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/src/views'));
 
 app.get('/', (req, res) => {
   res.render('index');
