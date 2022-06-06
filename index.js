c1863a062e9566dd28510d1f3fcb293cd8d4a76b
Thiago Allisson <thiago.ribeiro@ifma.edu.br>
2022-06-05 21:56:38 -0300

    Correções para deploy na vercel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import path from 'path';
 import {fileURLToPath} from 'url';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
