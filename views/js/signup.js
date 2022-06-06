e2439af64f8be9879de30d608466272e924ebcae
Thiago Allisson <thiago.ribeiro@ifma.edu.br>
2022-06-06 18:21:38 -0300

    Mudando nav-br e capturando dados de endereço

--- views/js/signup.js
+++ views/js/signup.js
@@ -1,15 +1,30 @@
-const navBrand = document.getElementsByClassName('nav-brand')[0];
-navBrand.addEventListener('click', () => {
-  window.location = './index';
-});
-
 const imageUpload = document.getElementsByClassName('img-upload')[0];
 imageUpload.addEventListener('click', () => {
   const inputFile = document.querySelector('input[type="file"');
   inputFile.click();
 });    
 
-const generateMap = (position) => {
+const getPosition = async (position) => {
+  const { latitude, longitude } = position.coords;  
+  const baseURL = 'https://nominatim.openstreetmap.org/reverse?';
+  const format = 'format=json';
+  //const zoom = 'zoom=21';
+  const addrDetails = 'addressdetails=1';
+  const apiURL = 
+    `${baseURL}&${format}&lat=${latitude}&lon=${longitude}&${addrDetails}`;
+  const locData = await fetch(apiURL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch(error => console.log(error));
+
+  return locData;
+}
+
+const generateMap = async (position) => {
   const { latitude, longitude } = position.coords;
   const map = new ol.Map({
     target: 'map',
@@ -23,13 +38,17 @@ const generateMap = (position) => {
       zoom: 16
     })
   });
-}
+
+  const data = await getPosition(position);
+  console.log(data.address);
+}   
 
 const getLocation = () => {
   if (navigator.geolocation) {
     let position = navigator.geolocation
-      .getCurrentPosition(generateMap, (error) => 
-        console.error(error)
+      .getCurrentPosition(
+        async (position) => await generateMap(position), 
+        (error) => console.error(error)
       );
   } else {
     console.error('Seu browser não suporta Geolocalização');
